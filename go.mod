b0c3e6a111326735a25cf2672433c760c61b63c6
Siew W.F <wingfei.siew@nova-hub.com>
2025-03-05 13:25:30 +0800

    i

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/MicahParks/keyfunc/v2 v2.1.0 // indirect
+	github.com/daaku/go.zipexe v1.0.2 // indirect
 	github.com/flosch/pongo2/v6 v6.0.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
@@ -23,9 +24,13 @@ require (
 	github.com/gofiber/template v1.8.3 // indirect
 	github.com/gofiber/utils v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mailru/easyjson v0.9.0 // indirect
+	github.com/pjdufour/go.rice v0.0.0-20180807042031-91c00aefc46a // indirect
+	github.com/spatialcurrent/go-template-loader v0.0.0-20170803032207-4765873f6e7e // indirect
 	github.com/swaggo/files v0.0.0-20220610200504-28940afbdbfe // indirect
+	github.com/ttacon/chalk v0.0.0-20160626202418-22c06c80ed31 // indirect
 	golang.org/x/net v0.35.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
 	golang.org/x/tools v0.30.0 // indirect
--- main.go
+++ main.go
@@ -5,13 +5,12 @@ import (
 	_ "app/docs"
 	"app/router"
 	"app/utils"
-
 	"github.com/gofiber/contrib/fiberzerolog"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/recover"
-	"github.com/swaggo/fiber-swagger"
 	"github.com/gofiber/template/django/v3"
+	"github.com/swaggo/fiber-swagger"
 )
 
 // @title Swagger App API
@@ -22,35 +21,47 @@ import (
 // @name Authorization
 // @BasePath /app
 func main() {
-	engine := django.New("./views", ".django")
-	utils.SetValidator()
-	utils.SetLogger()
-	app := fiber.New(fiber.Config{
-		Prefork: true,
-		Views: engine,
-	})
-	app.Use(recover.New())
-	app.Use(compress.New())
-	// app.Use("/app", cors.Config{AllowOrigins: "*"})
-	app.Use(fiberzerolog.New(fiberzerolog.Config{
+    engine := django.New("./views", ".django")
+    utils.SetValidator()
+    utils.SetLogger()
+    app := fiber.New(fiber.Config{
+        Prefork: true,
+        Views: engine,
+    })
+    app.Use(recover.New())
+    app.Use(compress.New())
+    // app.Use("/app", cors.Config{AllowOrigins: "*"})
+    app.Use(fiberzerolog.New(fiberzerolog.Config{
         Logger: &utils.Logger,
     }))
-	database.ConnectDB()
-	defer database.GetDb().Close()
+    database.ConnectDB()
+    defer database.GetDb().Close()
 
-	app.Get("/swagger/*", fiberSwagger.WrapHandler)
+    app.Get("/swagger/*", fiberSwagger.WrapHandler)
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		// Render index
-		return c.Render("file", fiber.Map{
-			"Title": "Hello, World!",
-		})
-	})
-	router.SetupRoutes(app)
+    app.Get("/app/index", func(c *fiber.Ctx) error {
+        // Render index
+        err := c.Render("invoice.xml", fiber.Map{
+            "inv": "INV1234598",
+            "issue_date": "2024-05-28",
+            "tin": "C5890633090",
+            "brn": "200201024235",
+        })
+        c.Set(fiber.HeaderContentType, fiber.MIMETextXML)
+        return err
+    })
+    app.Get("/app/test", func(c *fiber.Ctx) error {
+        agent := fiber.Get("http://localhost:8000/app/index")
+	    _, body, _ := agent.Bytes()
+        err := c.SendString(string(body[:]))
+        c.Set(fiber.HeaderContentType, fiber.MIMETextXML)
+        return err
+    })
+    router.SetupRoutes(app)
 
-	err := app.Listen(":8000")
+    err := app.Listen(":8000")
 
-	if err != nil {
-		utils.Logger.Fatal().Err(err).Msg("Fiber app error")
-	}
+    if err != nil {
+        utils.Logger.Fatal().Err(err).Msg("Fiber app error")
+    }
 }
